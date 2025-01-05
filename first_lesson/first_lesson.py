9512d2730f0742ab7d03782da8c97c78b7e03a46
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-05 21:10:02 +0200

    added until 8

--- first_lesson/first_lesson.py
+++ first_lesson/first_lesson.py
@@ -1,6 +1,8 @@
 yum = "helloworldiloveicecream"
 #      0123456789.....        #
 
+print(yum[4])
+
 ########### 1) print the character on index 5 ###################
 
 
@@ -10,10 +12,16 @@ yum = "helloworldiloveicecream"
 ########### 3) print(yum[1]) ###################
 
 
-
 ########### 4) print(yum[1:5]) ###################
-
 
 
 ########### 5) print(yum[10:15]) ###################
 
+
+########### 6) print the part of "worldilove" ###################
+
+
+########### 7) print the part of "helloworld" ###################
+
+
+########### 8) print the part of "icecream" ###################
