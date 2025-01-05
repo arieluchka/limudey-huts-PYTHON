5655d7bd93e6e8601b539c1654f3774e66c68b00
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-05 20:58:15 +0200

    added first_lesson file

---
+++ first_lesson/first_lesson.py
@@ -0,0 +1,17 @@
+yum = "helloworldiloveicecream"
+
+########### 1) print the character on index 5 ###################
+
+
+########### 2) what is the index of the letter "w" ###################
+
+
+########### 3) print(yum[1]) ###################
+
+
+
+########### 4) print(yum[1:5]) ###################
+
+
+
+########### 5) print(yum[10:15]) ###################
