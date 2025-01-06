2c4b7c38682ce253f01c6701ea3fcf86fca590dd
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-06 09:38:17 +0200

    added lists exercises

---
+++ first_lesson/home_work/02_lists.py
@@ -0,0 +1,37 @@
+# here is a list of US states
+state_names=["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+
+
+# 1) print the whole list
+
+
+# 2) print only the "California" state
+
+
+# 3) print only the "Arizona" state
+
+
+# 4) print all states from "Alaska" to "California" (including California)
+
+
+# 5) print the state at index 24
+
+
+# 6) print the last state on this list
+
+
+##############################################################################
+
+# 7) What do you think will happen if we do: print(state_names[0][3])
+# try it out!
+
+
+# 8) try to print the first letter of state on index 4
+# tip: first get the state, and then get to the index of last letter.
+
+
+# 9) Uncomment the next print and run it.
+print(state_names[10][0:2] + state_names[2][5:7] + " " + state_names[25])
+
+#  How do you think this happend? Try to break it down!
---
+++ first_lesson/home_work/03_more_lists!!!.py
@@ -0,0 +1,41 @@
+articles = ["The", "A", "An"]
+
+
+pronouns = ["I", "they", "them", "you", "she", "he"]
+
+
+nouns = ["dog", "cat", "mountain", "city", "computer", "teacher", "book", "house", "car", "child", "student"]
+
+
+verbs = ["runs", "jumps", "eat", "writes", "play", "sings", "sleeps", "read", "talks", "works"]
+
+
+adjectives = ["fast", "big", "happy", "sad", "bright", "smart", "small", "lazy", "beautiful", "strong"]
+
+
+adverbs = ["quickly", "slowly", "happily", "lazily", "carefully", "loudly", "gracefully", "well", "badly", "eagerly"]
+
+
+#################################################################################
+# we can use + to connect strings together
+# print("hello" + "world) = "helloworld"
+
+# we can pass multiple objects to print, seperated by ","
+# print("hello", "world") = "hello world"
+
+# print("hello" + " " + "world") = "hello world"
+# print("hello " + "world") = "hello world"
+#################################################################################
+
+# Try to print the next sentences:
+
+# 1) "I read well"
+
+# 2) "A dog runs quickly"
+
+# 3) "he writes lazily"
+
+# 4) "The car runs well"
+
+# 5) "A smart student works carefully"
+
