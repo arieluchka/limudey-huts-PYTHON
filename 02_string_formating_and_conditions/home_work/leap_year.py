cc55336a7f7482270767c1641a995bb3915a9f25
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-15 19:47:14 +0200

    added leap year exercise

---
+++ 02_string_formating_and_conditions/home_work/leap_year.py
@@ -0,0 +1,17 @@
+"""
+What is a leap year? (https://www.youtube.com/watch?v=xX96xng7sAE)
+##################################################################
+
+A leap year (in the Gregorian calendar) occurs:
+
+In every year that is evenly divisible by 4.
+Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly divisible by 400.
+
+
+Note:
+    - to solve this, you would need to use "Modulus" ("%")
+
+      https://www.w3schools.com/python/python_operators.asp#:~:text=Python%20Arithmetic%20Operators
+
+      https://www.geeksforgeeks.org/what-is-a-modulo-operator-in-python/
+"""
