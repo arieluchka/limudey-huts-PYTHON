001ce32375d45ee65934d4f9d27340f932c55953
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-21 18:20:38 +0200

    banking project

---
+++ 91_mini_projects/my_bank.py
@@ -0,0 +1,10 @@
+# [user_gov_id, balance]
+
+list_of_clients = [[123456789, 1000], [123123123, 500]]
+
+list_of_clients.append([4444444444, 100])
+
+print(list_of_clients)
+
+
+print(list_of_clients)
