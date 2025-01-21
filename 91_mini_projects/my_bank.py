cbda1c0850f46abbb80e736b028f6c1dea22e13f
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-21 18:23:15 +0200

    added instructions

--- 91_mini_projects/my_bank.py
+++ 91_mini_projects/my_bank.py
@@ -1,10 +1,14 @@
 # [user_gov_id, balance]
+#       0          1
 
 list_of_clients = [[123456789, 1000], [123123123, 500]]
 
 list_of_clients.append([4444444444, 100])
+list_of_clients.append([1241241353, 500])
 
 print(list_of_clients)
 
 
 print(list_of_clients)
+
+# 1) create a function, that will return the balance of the user, by user_gov_id
