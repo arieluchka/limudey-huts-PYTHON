df3d3e8dbfad76089b8a4c6618999c260d0a5a0f
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-01-22 18:46:44 +0200

    added banking file with more functions + updated data_types.py file

--- 91_mini_projects/my_bank.py
+++ 91_mini_projects/my_bank.py
@@ -1,14 +1,68 @@
-# [user_gov_id, balance]
-#       0          1
+# [client_first_name, client_last_name, user_gov_id, balance]
+#       0                     1              2         3
 
-list_of_clients = [[123456789, 1000], [123123123, 500]]
+# bank_clients_dict_format = {
+#     "user_id": str,
+#     "fisrt_name": str,
+#     "last_name": str,
+#     "balance": float
+# }
 
-list_of_clients.append([4444444444, 100])
-list_of_clients.append([1241241353, 500])
+list_of_clients = [
+    {"user_id": "124124135", "balance": 500, "first_name": "Hezi", "last_name": "Cohen"},
+    {"user_id": "322833344", "first_name": "Ariel", "last_name": "Agranovich", "balance": 500},
+    {"user_id": "444444444", "first_name": "Yedidiah", "last_name": "tzur", "balance": 100},
+]
 
-print(list_of_clients)
+# list_of_clients = [
+#     ["Hezi", "Cohen", "124124135", 500],
+#     ["Ariel", "Agranovich", "123123123", 500],
+#     ["Yedidiah", "tzur", "444444444", 100],
+#     ["Yaniv", "Rauper", "123456789", 1000],
+#     ["111111111", "Shir Sheli", "Eliasi", 100000000],
+# ]
 
 
-print(list_of_clients)
+
+# list_of_clients[0][0] = "Kobi"
+# print(list_of_clients)
+
+# print(list_of_clients)
+# print(list_of_clients)
+# user_bank_account = list_of_clients[1]
 
 # 1) create a function, that will return the balance of the user, by user_gov_id
+
+
+def return_balance(user_gov_id: str):
+    for client_account in list_of_clients:
+        if client_account["user_id"] == user_gov_id:
+            return client_account["balance"]
+
+
+def greet_client(user_gov_id: str):
+    for client_account in list_of_clients:
+        if client_account["user_id"] == user_gov_id:
+            print(f"Welcome back {client_account['first_name']}!")
+
+
+# Write a function that will deposit money to the account of the user
+def deposit_to_account(user_gov_id: str, amount: float):
+    pass
+
+
+# Write a function that will withdraw money from the account of the user
+def withdraw_from_account(user_gov_id: str, amount: float):
+    pass
+
+
+# Write a function that will transfer money from one account to another
+# IMPORTANT: check if the user has enough money
+def transfer_money(from_user_gov_id: str, to_user_gov_id: str, amount: float):
+    pass
+
+
+# Write a function that will add a new client to the bank
+# IMPORTANT: check if the user is already in the bank
+def add_new_client(user_gov_id: str, first_name: str, last_name: str, balance: float):
+    pass
--- first_lesson/data_types.py
+++ first_lesson/data_types.py
@@ -5,12 +5,12 @@
 # string - מחרוזת
 ####################################################################################
 
-var_1 = "hello world"
-#        0123456789....
-
-var_1_2 = 'hello world'
-var_1_3 = """hello world"""
-var_1_4 = '''hello world'''
+# var_1 = "hello world"
+# #        0123456789....
+#
+# var_1_2 = 'hello world'
+# var_1_3 = """hello world"""
+# var_1_4 = '''hello world'''
 
 
 # we can use a combination of " and ', to print them out as well
@@ -19,28 +19,28 @@ var_1_4 = '''hello world'''
 
 
 # how to access specific characters?
-print(var_1[0]) # print character on index 0
-print(var_1[5]) # print character on index 5
-print(var_1[2:5]) # print characters from index 2, up to index 5 (not including!)
-print(var_1[:5]) # print characters from the start up to index 5 (not including!)
-print(var_1[6:]) # print characters from index 6 until the end
+# print(var_1[0]) # print character on index 0
+# print(var_1[5]) # print character on index 5
+# print(var_1[2:5]) # print characters from index 2, up to index 5 (not including!)
+# print(var_1[:5]) # print characters from the start up to index 5 (not including!)
+# print(var_1[6:]) # print characters from index 6 until the end
 
 
 ####################################################################################
 # list - רשימות
 ####################################################################################
 
-var_2 = ["shalom", 2, 45, var_1]
+# var_2 = ["shalom", 2, 45, var_1]
 
-print(var_2)
+# print(var_2)
 
 
 # to access specific objects in a list is very similar to how we do it with strings
-print(var_2[2]) # print object in index 2
+# print(var_2[2]) # print object in index 2
 
 
 # we can also continue to get the index inside the indexed object!
-print(var_2[0][5])
+# print(var_2[0][5])
 # var_2[0] -> "shalom"[5] -> "m"
 #              012345
 
@@ -52,7 +52,7 @@ print(var_2[0][5])
 # נלמד בהמשך
 ####################################################################################
 
-var_3 = ("shalom", 2, 45, var_1)
+# var_3 = ("shalom", 2, 45, var_1)
 
 
 
@@ -65,9 +65,9 @@ var_3 = ("shalom", 2, 45, var_1)
 # int (integer) - מספר שלם
 ####################################################################################
 
-var_4 = 4235
+# var_4 = 4235
 
-print(var_4) # will still print "4235"
+# print(var_4) # will still print "4235"
 
 
 # we cant index ints!
@@ -75,13 +75,157 @@ print(var_4) # will still print "4235"
 
 
 # we can use math operations on ints
-print(5 * 3) #  = 15
-print("5" * 3)# = 555
+# print(5 * 3) #  = 15
+# print("5" * 3)# = 555
 
 
 
 ####################################################################################
 # float - מספר עשרוני
 ####################################################################################
-var_6 = 4235.6
+# var_6 = 4235.6
+
+
+
+
+#############################################################
+# Dictionary
+# {key: value, key: value, key: value}
+
+#!1) we cant have the same key twice
+
+# dict
+
+# car
+# car1 = {
+#     "color": "black",
+#     "price": 5000,
+#     "year": 2001,
+# }
+#
+#
+# car2 = {
+#     "color": "blue",
+#     "price": 15000,
+#     "year": 2018
+# }
+
+# Change the value of a key
+
+# print(car1)
+# car1["price"] = 4500
+# print(car1)
+
+
+# Add a new key: value
+
+# print(car1)
+#
+# car1["owner"] = "Ariel Agranovich"
+#
+# print(car1)
+#
+# car1["owner"] = "Shir"
+#
+# print(car1)
+
+
+# Delete a key: value
+
+# print(car1)
+
+# del car1["owner"]
+#
+# print(car1)
+#
+# if car1["owner"] == "Ariel":
+#     pass
+
+# dict methods
+
+car1 = {
+    "color": "black",
+    "price": [5000, 2000],
+    "year": 2001,
+}
+
+
+# print(car1["price"])
+
+# to get all values
+# print(car1.values())
+
+# to get all keys
+# print(car1.keys())
+
+# to get all items (key, value)
+# print(car1.keys())
+
+# for zoog in car1.items():
+#     print(f"{zoog[0]}: {zoog[1]}")
+
+
+# for val in car1.values():
+#     print(val)
+
+list1 = [
+    {
+        "color": "blue",
+        "price": [15000, 10000, 1500],
+        "year": 2018
+    },
+    {
+        "year": 2001,
+        "color": "black",
+        "price": 5000,
+    },
+    {
+        "year": 2001,
+        "color": "blue",
+        "price": 1234,
+    },
+
+]
+#
+#
+# for car in list1:
+#     if car["color"] == "blue":
+#         print(car["price"])
+#
+#
+#
+#
+# cars_dict = {
+#     1: car1,
+#     2: car2
+# }
+
+# print(car1["year"])
+#
+# print(car2["price"])
+#
+#
+# print(cars_dict[1])
+#
+#
+#
+#
+#
+#
+
+my_bank = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
