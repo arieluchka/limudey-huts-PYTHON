1338f3b931e3ff50a25f63f04a93f2aaf767ab08
sela.sc.ariel.agra <sela.sc.ariel.agra@BrightSourceEnergy.com>
2025-02-04 17:34:38 +0200

    added base functions file

---
+++ 91_mini_projects/boom_game/base_functions.py
@@ -0,0 +1,23 @@
+
+
+def devisible_by_7(num: int) -> bool:
+    pass
+
+
+def contains_7(num: int) -> bool:
+    pass
+
+
+
+if __name__ == '__main__':
+    print(f"The number 18 is devisible by 7 - {devisible_by_7(18)}")
+
+    print(f"The number 21 is devisible by 7 - {devisible_by_7(21)}")
+
+    print(f"The number 77 is devisible by 7 - {devisible_by_7(77)}")
+
+    print(f"The number 40 contains 7 - {contains_7(77)}")
+
+    print(f"The number 882412490907 contains 7 - {contains_7(77)}")
+
+    print(f"The number 6325290942 contains 7 - {contains_7(77)}")
